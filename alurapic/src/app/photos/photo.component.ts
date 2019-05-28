f69bbc4ee740cf83d1d3e30e02cdd4c1c977b64e
RenanSucs <47055395+RenanSucs@users.noreply.github.com>
2019-05-28 20:45:45 -0300

    ngFor Diretivas

---
+++ alurapic/src/app/photos/photo.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent{
+
+  @Input() description =''; //input = inbound properties, aceita receber valor fora do meu componente, direto no app.component
+  
+  @Input() url ='';
+}
