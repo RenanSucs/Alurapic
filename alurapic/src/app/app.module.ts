d93dc4a9faf70dd426f8514e9c2b80c299a406ea
RenanSucs <47055395+RenanSucs@users.noreply.github.com>
2019-05-28 21:16:42 -0300

    backend

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
-import { PhotoComponent } from './photos/photo.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PhotoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
