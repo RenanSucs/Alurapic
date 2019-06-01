ef90a4f963a4bfe4556a6b3fd0d78fe68230feb2
RenanSucs <47055395+RenanSucs@users.noreply.github.com>
2019-06-01 11:37:24 -0300

    Rotas dinamicas

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -9,14 +11,16 @@ import { PhotoService } from '../photo/photo.service';
 export class PhotoListComponent implements OnInit {
 
   photos: any[] = [];
-  constructor(private photoService: PhotoService){
-    
-  }
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+    ){}
   
   ngOnInit():void{
       
+    const userName = this.activatedRoute.snapshot.params.userName;
     this.photoService
-      .listFromUser('flavio')
+      .listFromUser(userName)
       .subscribe(photos => { 
         console.log(photos[5].description) 
         this.photos = photos});
