586ce8d70364d655d2d1003610c9efc46d04b9b2
RenanSucs <renansucsleite@hotmail.com>
2019-06-07 09:59:11 -0300

    Implementando resolve

---
+++ alurapic/src/app/photos/photo-list/photo-list.resolver.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { Resolve } from "@angular/router";
+import { Observable } from "rxjs";
+
+import { PhotoService } from "../photo/photo.service";
+import { Photo } from "../photo/photo";
+
+@Injectable({ providedIn: "root" })
+export class PhotoListResolver implements Resolve<Observable<Photo[]>>{
+    
+    resolve(route: import("@angular/router").ActivatedRouteSnapshot, state: import("@angular/router").RouterStateSnapshot): Observable<Photo[]> | Observable<Observable<Photo[]>> | Promise<Observable<Photo[]>> {
+        
+        const userName = route.params.userName;
+        return this.service.listFromUser(userName);
+    }
+
+    constructor(private service: PhotoService) {}
+}
