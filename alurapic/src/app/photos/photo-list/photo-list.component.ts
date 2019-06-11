3e99de0a1c3948a78c1f0eafafda3d55dae3b9d7
RenanSucs <renansucsleite@hotmail.com>
2019-06-11 09:24:09 -0300

    debounceTime

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
@@ -12,11 +14,16 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute){}
   
   ngOnInit():void{
       this.photos = this.activatedRoute.snapshot.data.photos;
+
+     this.debounce
+     .pipe(debounceTime(300))
+     .subscribe(filter => this.filter = filter);
   }
 
 }
